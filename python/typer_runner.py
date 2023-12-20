8ec2c5dbcddcfb618acf8b7b2fc83db27495570f
ereznaim <erez.n@imagen-ai.com>
2023-12-20 16:52:29 +0200

    Adding the following html files: 1. index.html 2. success.html 3. flask_python.py
    modify typer_runner.py to support flask operations

--- python/typer_runner.py
+++ python/typer_runner.py
@@ -1,29 +1,70 @@
 from typing import Optional
-
+import os
 import typer
-
+from InquirerPy import prompt, inquirer
 from api_client import ImagenAPIClient, MissingAPIKeyException
 
 app = typer.Typer()
 
 
+def get_folder_path(message: str) -> str:
+    return inquirer.text(
+        message=message,
+        validate=lambda input: os.path.isdir(input),
+    ).execute()
+
+
+def get_input(prompt_message: str) -> str:
+    return inquirer.text(message=prompt_message).execute()
+
+
+def get_boolean_choice(parameter_name: str):
+    questions = [
+        {
+            "type": "list",
+            "name": "choice",
+            "message": f"Do you want to enable {parameter_name}?",
+            "choices": ["Yes", "No"],
+        },
+    ]
+    answers = prompt(questions)
+    return answers['choice'] == "Yes"
+
+    # @app.command()
+    # def run(
+    #         input_dir: str = typer.Option(..., "--input_dir", help="Path to the input directory",
+    #                                       prompt="Please insert your image for editing folder name/location"),
+    #         output_dir: str = typer.Option(..., "--output_dir", help="Path to the output directory",
+    #                                        prompt="Please insert your image edited output folder name/location"),
+    #         profile_name: Optional[str] = typer.Option(None, "--profile_name", help="Name of the profile",
+    #                                                    prompt="Please insert your profile project name"),
+    #         api_key: Optional[str] = typer.Option(None, "--api_key", help="API key",
+    #                                               prompt="Please insert your unique API Key"),
+    #         callback_url: Optional[str] = typer.Option(None, "--callback_url", help="Callback URL"),
+    #         profile_key: Optional[str] = typer.Option(None, "--profile_key", help="The key of the profile"),
+    #         hdr_merge: bool = typer.Option(False, "--hdr_merge", help="Use HDR merge"),
+    #         crop: bool = typer.Option(False, "--crop", help="Use crop"),
+    #         straighten: bool = typer.Option(False, "--straighten", help="Use straighten"),
+    #         subject_mask: bool = typer.Option(False, "--subject_mask", help="Use subject mask"),
+    #         export: bool = typer.Option(False, "--export", help="Export to jpg")):
+
+
 @app.command()
 def run(
-        input_dir: str = typer.Option(..., "--input_dir", help="Path to the input directory",
-                                      prompt="Please insert your image for editing folder name/location"),
-        output_dir: str = typer.Option(..., "--output_dir", help="Path to the output directory",
-                                       prompt="Please insert your image edited output folder name/location"),
-        profile_name: Optional[str] = typer.Option(None, "--profile_name", help="Name of the profile",
-                                                   prompt="Please insert your profile project name"),
-        api_key: Optional[str] = typer.Option(None, "--api_key", help="API key",
-                                              prompt="Please insert your unique API Key"),
         callback_url: Optional[str] = typer.Option(None, "--callback_url", help="Callback URL"),
         profile_key: Optional[str] = typer.Option(None, "--profile_key", help="The key of the profile"),
-        hdr_merge: bool = typer.Option(False, "--hdr_merge", help="Use HDR merge"),
-        crop: bool = typer.Option(False, "--crop", help="Use crop"),
-        straighten: bool = typer.Option(False, "--straighten", help="Use straighten"),
-        subject_mask: bool = typer.Option(False, "--subject_mask", help="Use subject mask"),
         export: bool = typer.Option(False, "--export", help="Export to jpg")):
+
+    input_dir = get_folder_path("Please insert your image for editing folder name/location")
+    output_dir = get_folder_path("Please insert your image edited output folder name/location")
+
+    profile_name = get_input("Please insert your profile project name")
+    api_key = get_input("Please insert your unique API Key")
+
+    hdr_merge = get_boolean_choice("HDR merge")
+    crop = get_boolean_choice("crop")
+    straighten = get_boolean_choice("straighten")
+    subject_mask = get_boolean_choice("subject mask")
     """
     This script processes images using the Imagen AI API. It allows users to specify input and output directories,
     profile name, API key, and an optional callback URL. It handles the uploading, editing, and downloading of images.
